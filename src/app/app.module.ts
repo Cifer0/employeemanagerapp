a9c2491396616fe6a4fd8a36b8dbdab5ca7c905b
Cifer0 <66801538+Cifer0@users.noreply.github.com>
2021-10-06 13:58:43 +0200

    custom UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// Import HttpClientModule from @anular/common/http in AppModule
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
+// Import HttpClientModule from @anular/common/http in AppModule
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
